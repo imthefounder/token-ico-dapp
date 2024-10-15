bbb8ebea45e6cc920d1985c05c2654b8bff45890
Hash <dannyhash0x1@gmail.com>
2024-10-15 06:24:51 +0100

    Created contract modifier

--- contracts/TokenICO.sol
+++ contracts/TokenICO.sol
@@ -11,3 +11,52 @@ interface ERC20 {
     function totalSupply() external view returns(uint256);
     function name() external view returns(string memory);
 }
+
+contract TokenICO {
+    address public owner;
+    address public tokenAddress;
+    uint256 public tokenSalePrice;
+    uint256 public soldTokens;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only contract owner can perform this operation");
+        _;
+    }
+
+    constructor() {
+
+    }
+
+    function updateToken() {
+        
+    }
+
+    function updateTokenSalePrice() {
+
+    }
+
+    function multiply() {
+
+    }
+
+    function buyToken() {
+
+    }
+
+    function getTokenDetails() {
+
+    }
+
+    function transferToOwner() {
+
+    }
+
+    function transferEther() {
+
+    }
+
+    function withdrawAllTokens() {
+
+    }
+    
+}
