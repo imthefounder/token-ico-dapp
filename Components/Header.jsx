794b86690eb7816fa25f2709f56bcd65f87280d9
Hash <dannyhash0x1@gmail.com>
2024-10-23 12:42:42 +0100

    Feat: added MetaMask detection and accountsChanged event listener in useEffect in Header component

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Header = () => {
+const Header = ({
+  account,
+  CONNECT_WALLET,
+  setAccount,
+  setLoader,
+  setOwnerModel,
+  shortenAddress,
+  detail,
+  currency,
+  ownerModel,
+}) => {
+  const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(false);
+
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      setIsMetamaskInstalled(true);
+
+      window.ethereum.on("accountsChanged", handleAccountsChanged);
+    }
+    return () => {
+      if (typeof window.ethereum !== "undefined") {
+        window.ethereum.removeListener(
+          "accountsChanged",
+          handleAccountsChanged
+        );
+      }
+    };
+  }, []);
+
   return <div>Header</div>;
 };
 
