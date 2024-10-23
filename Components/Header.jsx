b169bfb1df6419cff345078fdba99cffe14a2851
Hash <dannyhash0x1@gmail.com>
2024-10-23 17:07:25 +0100

    Feat: implemented header layout with navigation, wallet connection, and clipboard functionality

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -49,7 +49,108 @@ const Header = ({
     }
   };
 
-  return <div>Header</div>;
+  return (
+    <header className="site-header header--transparent ico-header">
+      <div className="header__main-wrap">
+        <div className="container mxw_1640">
+          <div className="header__main ul_li_between">
+            <div className="header__left ul_li">
+              <div className="header__logo">
+                <a href="/">
+                  <img src="assets/img/logo/logo.svg" alt="" srcset="" />
+                </a>
+              </div>
+            </div>
+
+            <div className="main-menu__wrap ul_li navbar navbar-expand-xl">
+              <nav className="main-menu collapse navbar-collapse">
+                <ul>
+                  <li className="active has-mega-menu">
+                    <a href="/">Home</a>
+                  </li>
+
+                  <li>
+                    <a className="scrollspy-btn" href="#about">
+                      About
+                    </a>
+                  </li>
+
+                  <li>
+                    <a className="scrollspy-btn" href="#roadmap">
+                      Roadmap
+                    </a>
+                  </li>
+
+                  <li>
+                    <a className="scrollspy-btn" href="#team">
+                      Team
+                    </a>
+                  </li>
+
+                  <li>
+                    <a className="scrollspy-btn" href="#faq">
+                      Faq
+                    </a>
+                  </li>
+
+                  <li>
+                    <a className="scrollspy-btn" href="#contact">
+                      Contact
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="scrollspy-btn"
+                      style={{
+                        cursor: "pointer",
+                      }}
+                      onClick={() =>
+                        ownerModel ? setOwnerModel(false) : setOwnerModel(true)
+                      }
+                    >
+                      Tools
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="header__action ul_li">
+              <div className="d-xl-none">
+                <a className="header__bar hamburger__menu">
+                  <div className="header__bar-icon">
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                  </div>
+                </a>
+              </div>
+
+              {account ? (
+                <div className="header__account">
+                  <a
+                    onClick={() =>
+                      navigator.clipboard.writeText(detail?.address)
+                    }
+                  >
+                    {shortenAddress(detail?.address)}:{" "}
+                    {detail?.maticBal ? detail.maticBal.slice(0, 6) : "0.00"}
+                    {currency}
+                  </a>
+                </div>
+              ) : (
+                <div className="header__account">
+                  <a onClick={connectMetamask}>Connect wallet</a>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
